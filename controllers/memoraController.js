904160935867f79354c977dd7d3747a287571c3b
aman.b <aman.bhandari@grazitti.com>
2025-09-03 01:36:56 +0530

    sending user uid to ML service

--- controllers/memoraController.js
+++ controllers/memoraController.js
@@ -9,12 +9,12 @@ const { ObjectId } = mongoose;
 // BELOW FUNCTION SENDS THE IMAGE TO PYTHON ML SERVICE TO GENERATE CAPTION
 
 
-const generateCaption = async (filePath, fileName) => {
+const generateCaption = async (filePath, fileName, user_uid) => {
     try {
         console.log('dafaakkk--')
         const signedURL = await generateSignedUrl(fileName)
         console.log("ab kya chuda----->", signedURL)
-        const response = await axios.post(process.env.ML_SERVICE_URL + "/generate-caption", { filePath: signedURL, fileName })
+        const response = await axios.post(process.env.ML_SERVICE_URL + "/generate-caption", { filePath: signedURL, fileName ,user_uid})
 
         return response.data.caption
     } catch (err) {
@@ -33,7 +33,7 @@ const addMemory = async (req, res) => {
         console.log("titelle---", Uploadedimage, title)
         console.log('filekey---->', req.file.key)
         console.log('uid bc---->', req.body.user_id)
-        description = await generateCaption(req.file.location, req.file.key)
+        description = await generateCaption(req.file.location, req.file.key,req.body.user_id)
         // description='Temp description'
         console.log("got here-----image--->", description)
 
@@ -75,7 +75,7 @@ const getUserAllMemory = async (req, res) => {
 
 const searchUserQuery = async (req, res) => {
     try {
-        console.log('herere-----')
+        console.log('herere-----',req.body.query,req.body.user_id)
         const response = await axios.post(process.env.ML_SERVICE_URL + "/search-image-vector", { query: req.body.query, user_id: req.body.user_id })
 
 
